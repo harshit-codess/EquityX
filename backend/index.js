d56d96c641ea81d5256828a096e2128186a7687f
Harshit-codess <hr6087410@gmail.com>
2026-08-03 10:40:45 +0530

    cors chnage

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,7 @@ const url = process.env.MONGO_URL;
 
 const app = express();
 
-app.use(cors({
-    origin: [  process.env.FRONTEND_URL,
-      process.env.DASHBOARD_URL,
-    ],
-    credentials: true,
-  }));
+app.use(cors());
 app.use(bodyParser.json());
 
 const cookieParser = require('cookie-parser');
